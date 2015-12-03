b889d42a98a16ee326ba0dc8c7a5abfdb84ec600
iwzh <1145769693@qq.com>
2015-12-03 09:42:46 +0800

    修复vote投票插件的bug 修复互动应用点击bug 修复重装后vote插件不安装数据的bug
    Signed-off-by: iwzh <1145769693@qq.com>

--- Addons/Vote/VoteAddon.class.php
+++ Addons/Vote/VoteAddon.class.php
@@ -31,10 +31,18 @@ use Common\Controller\Addon;
         );
 
         public function install(){
+            $install_sql = './Addons/Vote/install.sql';
+            if (file_exists ( $install_sql )) {
+                execute_sql_file ( $install_sql );
+            }
             return true;
         }
 
         public function uninstall(){
+            $uninstall_sql = './Addons/Vote/uninstall.sql';
+            if (file_exists ( $uninstall_sql )) {
+                execute_sql_file ( $uninstall_sql );
+            }
             return true;
         }
 
